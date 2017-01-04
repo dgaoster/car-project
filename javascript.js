8d900bf93511ea52c6699579b405f7aa9b53d93e
dgaoster <davidgao96@yahoo.com>
2017-01-03 20:07:00 -0800

    started implementing autocompletion textbox

--- javascript.js
+++ javascript.js
@@ -1,8 +1,5 @@
-/*
- * - Define variables
- * - Calculate the document height and set the offset to a quarter of that value
- * - Add the event listeners for scroll and click
- */
+// BACK TO TOP BUTTON
+
 var btt = document.getElementById('back-to-top'),
     body = document.body,
     docElem = document.documentElement,
@@ -28,3 +25,118 @@ btt.addEventListener('click', function(event) {
         body.scrollTop = 0;
     }
 });
+
+
+// AUTOCOMPLETE
+
+var targetInput = document.getElementById('car-mm'),
+    results = document.getElementById('autocomplete-results'),
+    carList = ['Toyota Camry', 'Toyota Sienna', 'Lexus ES350', 'Filler', 'Test', 'Apple', 'Banana'],
+    matches = [],
+    resultsCursor = 0;
+
+// Focus the input
+targetInput.focus();
+
+// Add event listener for the input keydown
+targetInput.addEventListener('keydown', function(event) {
+    if (event.keyCode == '13') {
+        event.preventDefault();
+    }
+});
+
+// Add event listener for the input keyup
+targetInput.addEventListener('keyup', function(event) {
+    /*
+    * Key codes
+    *
+    * Enter: 13
+    * Arrow up: 38
+    * Arrow down: 40
+    */
+
+    results.innerHTML = '';
+    toggleResults('hide');
+
+    if (this.value.length > 0) {
+        matches = getMatches(this.value);
+
+        if (matches.length > 0) {
+            displayMatches(matches);
+        }
+    }
+
+    if (results.classList.contains('visible')) {
+        switch(event.keyCode) {
+            case 13:
+                targetInput.value = results.children[resultsCursor].innerHTML;
+                toggleResults('hide');
+                resultsCursor = 0;
+
+                break;
+            case 38:
+                if (resultsCursor > 0) {
+                    resultsCursor--;
+
+                    moveCursor(resultsCursor);
+                }
+
+                break;
+            case 40:
+                if (resultsCursor < (matches.length - 1)) {
+                    resultsCursor++;
+
+                    moveCursor(resultsCursor);
+                }
+
+                break;
+        }
+    }
+});
+
+// Define a function for checking if the input value matches any of the car names
+function getMatches(inputText) {
+    var matchList = [];
+
+    for (var i = 0; i < carList.length; i++) {
+        if (carList[i].toLowerCase().indexOf(inputText.toLowerCase()) != -1) {
+            matchList.push(carList[i]);
+        }
+    }
+
+    return matchList;
+}
+
+// Define a function for displaying autocomplete results
+function displayMatches(matchList) {
+    var j = 0;
+
+    while (j < matchList.length) {
+        results.innerHTML += '<li class="result">' + matchList[j] + '</li>';
+        j++;
+    }
+
+    // The first child gets a class of "highlighted"
+    moveCursor(resultsCursor);
+
+    // Show the results
+    toggleResults('show');
+}
+
+// Define a function for moving the cursor in the results list
+function moveCursor(pos) {
+    for (var x = 0; x < results.children.length; x++) {
+        results.children[x].classList.remove('highlighted');
+    }
+
+    results.children[pos].classList.add('highlighted');
+}
+
+// Define a function for toggling the results list
+function toggleResults(action) {
+    if (action == 'show') {
+        results.classList.add('visible');
+    } else if (action == 'hide') {
+        results.classList.remove('visible');
+    }
+}
